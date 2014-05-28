ebc2a917568aa2f34c93ca2a6a68bf462079679a
jfischetti94@gmail.com <jfischetti94@gmail.com@c5aa61bc-f277-0c37-9071-a6a8f5030ec8>
2014-05-28 00:46:11 +0000

    Creacion Clase Stock con todos los metodos completos sin comentar. Creacion Clase Producto sin nada.

--- trunk/SistemaDeVentas/src/producto/Stock.java
+++ trunk/SistemaDeVentas/src/producto/Stock.java
@@ -1,5 +1,57 @@
 package producto;
 
 class Stock {
+	int stock;
+	int critico;
+	int minimo;
 
+	protected Stock(int stock, int critico, int minimo) {
+		this.stock = stock;
+		this.critico = critico;
+		this.minimo = minimo;
+	}
+	
+	private int getStock() {
+		return stock;
+	}
+
+	private void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	private int getCritico() {
+		return critico;
+	}
+
+	private void setCritico(int critico) {
+		this.critico = critico;
+	}
+
+	private int getMinimo() {
+		return minimo;
+	}
+
+	private void setMinimo(int minimo) {
+		this.minimo = minimo;
+	}
+
+	public int stock() {
+		return this.getStock();
+	}
+
+	public boolean enStock(int cantidad) {
+		return this.getStock() >= cantidad;
+	}
+
+	public void decrementarStock(int cantidad) {
+		this.setStock(this.getStock() - cantidad);
+	}
+
+	public int critico() {
+		return this.getCritico();
+	}
+
+	public int minimo() {
+		return this.getMinimo();
+	}
 }
