9ac1e6511ff76db71388779f0ce2a1edde6889c0
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@c5aa61bc-f277-0c37-9071-a6a8f5030ec8>
2014-05-29 12:37:42 +0000

    Probando desde mi pc, agregue un par de variables de instancia y un metodo sin cuerpo

--- SistemaDeVentas/src/producto/Producto.java
+++ SistemaDeVentas/src/producto/Producto.java
@@ -2,4 +2,14 @@ package producto;
 
 public class Producto {
 
+	Presentacion presentacion;
+	String unicacion;
+	String nombre;
+	String marca;
+	String descripcion;
+	
+	
+	public void decrementarStock(Presentacion p, int cant){
+		
+	}
 }
