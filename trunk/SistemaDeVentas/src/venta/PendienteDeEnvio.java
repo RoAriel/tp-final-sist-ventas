df046eac7780dd55b6b8eeb72a0d42762b9bd97c
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@c5aa61bc-f277-0c37-9071-a6a8f5030ec8>
2014-06-12 22:47:54 +0000

    Cree la clase Presentacion (vacia) Me meti en el envio, creando un paquete envio el cual contiene a la clase venta

---
+++ trunk/SistemaDeVentas/src/venta/PendienteDeEnvio.java
@@ -0,0 +1,20 @@
+package venta;
+
+public class PendienteDeEnvio extends EstadoEnvio {
+
+	public void enviar(Envio env) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void cancelar(Envio env) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void reporgramar(Envio env) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
