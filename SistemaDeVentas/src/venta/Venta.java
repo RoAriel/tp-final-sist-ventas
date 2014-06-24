d2a055b5234d98165f7f2dfe9ae2fe5798350c75
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@c5aa61bc-f277-0c37-9071-a6a8f5030ec8>
2014-06-24 03:11:02 +0000

    - Comportamiento de la Clase VentaDirecta casi terminado - Agregue un paquete para las Excepciones de Ventas              - AccionInvalidaException              - VentaDirectaStockInvalidException

--- SistemaDeVentas/src/venta/Venta.java
+++ SistemaDeVentas/src/venta/Venta.java
@@ -8,8 +8,39 @@ public class Venta {
 
 	private List<Presentacion> productos;
 	private List<Presentacion> productosSinStock;
-	private Cliente cliente;	
+	private Cliente cliente;
 	private FormaDePago formaDePago;
-	
-	
+
+	public List<Presentacion> getProductos() {
+		return productos;
+	}
+
+	public List<Presentacion> getProductosSinStock() {
+		return productosSinStock;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public FormaDePago getFormaDePago() {
+		return formaDePago;
+	}
+
+	private void setProductos(List<Presentacion> productos) {
+		this.productos = productos;
+	}
+
+	private void setProductosSinStock(List<Presentacion> productosSinStock) {
+		this.productosSinStock = productosSinStock;
+	}
+
+	private void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	private void setFormaDePago(FormaDePago formaDePago) {
+		this.formaDePago = formaDePago;
+	}
+
 }
