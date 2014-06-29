f715a94fe8fb1d74eedce822e87d8b9f344e085a
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@c5aa61bc-f277-0c37-9071-a6a8f5030ec8>
2014-06-29 13:29:13 +0000

    Esqueleto de VentaDirectaTest (prueba)

---
+++ trunk/SistemaDeVentas/test/venta/VentaDirectaTest.java
@@ -0,0 +1,14 @@
+package venta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VentaDirectaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
